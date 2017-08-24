6b34264336ff7a773079849b4c8b38269c1f6640
Lionel Henry <lionel.hry@gmail.com>
2017-08-24 10:46:15 +0200

    Refactor deprecation tests

--- tests/testthat/test-signal-deprecation.R
+++ tests/testthat/test-signal-deprecation.R
@@ -1,5 +1,8 @@
 context("signal")
 
+defunct_cycle <- c("", "", as.character(past_rlang_ver()))
+rlang_fn <- set_env(function() "returned", ns_env("rlang"))
+
 test_that("correct deprecation message is built", {
   cycle <- c("1.0.0", "2.0.0", "3.0.0")
   expect_identical(deprecated_function_msg("pkg::foo()", "1.0.0", 1, "bar()"), "`pkg::foo()` is soft-deprecated as of version 1.0.0, please use `bar()` instead")
@@ -8,44 +11,35 @@ test_that("correct deprecation message is built", {
 })
 
 test_that("deprecated function does not signal if not in deprecation cycle", {
-  foo <- function() "returned"
-  foo <- set_env(foo, ns_env("rlang"))
-
   future_ver <- as.character(future_rlang_ver())
-  foo <- deprecate(foo, c(future_ver, "", ""))
+  oldie <- deprecate(rlang_fn, c(future_ver, "", ""))
 
-  expect_true(is_deprecated(foo))
-  expect_condition(foo(), NA)
+  expect_true(is_deprecated(oldie))
+  expect_condition(oldie(), NA)
 })
 
 test_that("deprecated function signals soft-deprecation", {
-  foo <- function() "returned"
-  foo <- set_env(foo, ns_env("rlang"))
-
   past_ver <- as.character(past_rlang_ver())
-  foo <- deprecate(foo, c(past_ver, "", ""))
+  oldie <- deprecate(rlang_fn, c(past_ver, "", ""))
 
   msg <- sprintf("deprecated as of version %s", past_ver)
-  expect_condition(foo(), "deprecated", msg)
+  expect_condition(oldie(), "deprecated", msg)
 })
 
 test_that("deprecated function signals deprecation", {
-  foo <- function() "returned"
-  foo <- set_env(foo, ns_env("rlang"))
-
   past_ver <- as.character(past_rlang_ver())
-  foo <- deprecate(foo, c("", past_ver, ""))
+  oldie <- deprecate(rlang_fn, c("", past_ver, ""))
 
   msg <- sprintf("deprecated as of version %s", past_ver)
-  expect_warning(foo(), msg)
+  expect_warning(oldie(), msg)
 })
 
 test_that("deprecated function signals itself defunct", {
-  foo <- function() "returned"
-  foo <- set_env(foo, ns_env("rlang"))
-
   past_ver <- as.character(past_rlang_ver())
-  foo <- deprecate(foo, c("", "", past_ver))
+  oldie <- deprecate(rlang_fn, c("", "", past_ver))
+
+  expect_error(oldie(), "defunct as of version 0.1.0")
+})
 
   expect_error(foo(), "defunct as of version 0.1.0")
 })
